7db8474f18748aa8833596d89cd1eaf98cef24c2
Duong Duc <vietduc9596@gmail.com>
2023-12-23 21:38:19 +0700

    Update i18n

--- src/main/java/com/fpt/assigngeneral/config/AppConfig.java
+++ src/main/java/com/fpt/assigngeneral/config/AppConfig.java
@@ -1,7 +1,5 @@
 package com.fpt.assigngeneral.config;
 
-import java.util.Locale;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,33 +10,58 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
+
+/**
+ * Configuration class for setting up internationalization and localization in the Spring Boot application.
+ */
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
-  @Bean
-  LocaleResolver localeResolver() {
-    SessionLocaleResolver resolver = new SessionLocaleResolver();
-    resolver.setDefaultLocale(new Locale("vi"));
-    return resolver;
-  }
+    /**
+     * Configures the locale resolver bean.
+     *
+     * @return The configured {@link LocaleResolver} bean.
+     */
+    @Bean
+    LocaleResolver localeResolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(new Locale("vi"));
+        return resolver;
+    }
 
-  @Bean
-  LocaleChangeInterceptor localeChangeInterceptor() {
-    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-    lci.setParamName("lang");
-    return lci;
-  }
+    /**
+     * Configures the locale change interceptor bean.
+     *
+     * @return The configured {@link LocaleChangeInterceptor} bean.
+     */
+    @Bean
+    LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
 
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(localeChangeInterceptor());
-  }
-  
-  @Bean
-  MessageSource messageSource() {
-    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-    messageSource.setBasename("messages\\message");
-    messageSource.setDefaultEncoding("UTF-8");
-    return messageSource;
-  }
+    /**
+     * Adds the locale change interceptor to the interceptor registry.
+     *
+     * @param registry The {@link InterceptorRegistry} to which the locale change interceptor is added.
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    /**
+     * Configures the message source bean for internationalization.
+     *
+     * @return The configured {@link MessageSource} bean.
+     */
+    @Bean
+    MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages\\message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
