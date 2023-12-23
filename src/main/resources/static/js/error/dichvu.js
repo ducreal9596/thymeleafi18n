6173463bac5a41b5ba30d9e008e1dae912d9f4ef
Duong Duc <vietduc9596@gmail.com>
2023-12-23 21:27:43 +0700

    Update i18n

--- src/main/resources/static/js/error/dichvu.js
+++ src/main/resources/static/js/error/dichvu.js
@@ -1,6 +1,6 @@
 let error = [
-	{ index: 1, message: "Khong duoc de trong", regex: /^DV\d{3}$/ },
-	{ index: 2, message: "Khong duoc de trong", regex: /^[a-zA-Z]/ },
-	{ index: 3, message: "Khong duoc de trong", regex: /^[a-zA-Z]/ },
-	{ index: 4, message: "Khong duoc de trong", regex: /^[0-9]/ },
+	{ index: 1, message: /*[[#{error.required}]]*/'', regex: /^DV\d{3}$/ },
+	{ index: 2, message: /*[[#{error.alpha}]]*/'', regex: /^[a-zA-Z]/ },
+	{ index: 3, message: /*[[#{error.alpha}]]*/'', regex: /^[a-zA-Z]/ },
+	{ index: 4, message: /*[[#{error.numeric}]]*/'', regex: /^[0-9]/ },
 ];
